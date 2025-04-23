db9c6bbf8fd9f91ca6a5045e8826869415e6e794
mandla Tivane <smangamandla05@gmail.com>
2025-04-23 23:34:19 +0200

    intial commit

---
+++ src/gitassignmentproject/GitAssignmentProject.java
@@ -0,0 +1,9 @@
+ 
+package gitassignmentproject;
+
+ 
+public class GitAssignmentProject {
+    public static void main(String[] args) {
+        System.out.println("Hello NetBeans. Welcome to version control using Git.");
+    }
+}
